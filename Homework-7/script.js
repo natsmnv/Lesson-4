60f357f8c8543448edefeffbf9e02469bcd56867
natsmnv <natnatanatasha@gmail.com>
2023-03-24 19:30:54 +0200

    Add changes

--- Homework-7/script.js
+++ Homework-7/script.js
@@ -1,27 +1,14 @@
-let numOrStr = prompt('input number or string');
-console.log(numOrStr)
-
-if(numOrStr === null) {
-console.log('if. ви скасували')
-} else if( numOrStr.trim() === '' ) {
-console.log('if. Empty String');
-} else if ( isNaN( +numOrStr ) ) {
-console.log('if.  number is Ba_NaN')
-} else {
-console.log('if. OK!')
-}
-
 switch(numOrStr) {
     case null:
-        console.log('switch. You canceled');
+        console.log('You canceled');
         break;
     case '':
-        console.log('switch. Empty String');
+        console.log('Empty String');
         break;
     case isNaN(+numOrStr):
-        console.log('switch. number is Ba_NaN');
+        console.log('number is Ba_NaN');
         break;
     default:
-        console.log('switch. OK!');
+        console.log('OK!');
         break;
 }
