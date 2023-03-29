ce9f0163d9bb8db36451078f6d1d37835a57ef11
natsmnv <natnatanatasha@gmail.com>
2023-03-29 14:58:03 +0300

    Made code more readable

--- Homework-6/script.js
+++ Homework-6/script.js
@@ -4,11 +4,24 @@ let sport = prompt('What is your favorite type of sport?');
 const yearNow = 2023;
 let age = yearNow - yearOfBirth;
 
-alert(`${yearOfBirth === 0 ? 'Why don`t you enter your year of birth?' : `You are ${yearNow - yearOfBirth} years old.`}
-${city === null ? 'Why don`t you enter your city?' : city === 'Kyiv' ? `You live in the capital ${city}.`
-: city === 'Washington' ? `You live in the capital ${city}.` : city === 'London' ? `You live in the capital ${city}.`
-: `You live in city ${city}.`}
-${sport === null ? 'Why don`t enter your favorite type of sport?' : sport === 'boxing' 
-? 'Cool! Do you want to be an Oleksandr Usyk?'
-: sport === 'swimming' ? 'Cool! Do you want to be an Yana Klochkova?' : sport === 'gymnastics'
-? 'Cool! Do you want to be a Lilia Podkopayeva?' : `Your favorite type of sport is ${sport}.`}`);
+alert(`${yearOfBirth === 0 
+    ? 'Why don`t you enter your year of birth?' 
+    : `You are ${yearNow - yearOfBirth} years old.`}
+    ${city === null 
+    ? 'Why don`t you enter your city?' 
+    : city === 'Kyiv' 
+    ? `You live in the capital ${city}.`
+    : city === 'Washington' 
+    ? `You live in the capital ${city}.` 
+    : city === 'London' 
+    ? `You live in the capital ${city}.`
+    : `You live in city ${city}.`}
+    ${sport === null 
+    ? 'Why don`t enter your favorite type of sport?' 
+    : sport === 'boxing' 
+    ? 'Cool! Do you want to be an Oleksandr Usyk?'
+    : sport === 'swimming' 
+    ? 'Cool! Do you want to be an Yana Klochkova?' 
+    : sport === 'gymnastics'
+    ? 'Cool! Do you want to be a Lilia Podkopayeva?' 
+    : `Your favorite type of sport is ${sport}.`}`);
